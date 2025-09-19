325e57b311995ee11266fbba2c1935a43858f790
Sebastiano-ITS <sebastiano.peluso@edu-its.it>
2025-09-19 12:02:58 +0200

    fatto login con sharedpreferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    kotlin("plugin.serialization") version "1.9.23" // Added for Kotlinx Serialization
 }
 
 android {
@@ -41,46 +40,27 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
 
+    // Dipendenze per Jetpack Compose Material 3
+    implementation(libs.material3)
+    implementation(libs.androidx.material3.window.size.class1)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.compose.animation)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-
-    implementation(libs.androidx.material.icons.core)     // Changed to catalog
-    implementation(libs.androidx.material.icons.extended) // Changed to catalog
-
-    // Navigation
-    implementation(libs.androidx.navigation.compose)      // Changed to catalog
-
-    // AppCompat
-    implementation(libs.androidx.appcompat)               // Changed to catalog
-
-    // Kotlinx Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3") // Added for Kotlinx Serialization
-
-    // Jetpack DataStore (Preferences)
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
-
-    // Google Tink (Android)
-    implementation("com.google.crypto.tink:tink-android:1.13.0")
-
-    // implementation("androidx.security:security-crypto:1.1.0") // REMOVED - Replaced by DataStore + Tink
-
-    // Test Implementation
+    implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-
-    // Debug Implementation
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
